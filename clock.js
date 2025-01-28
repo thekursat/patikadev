ff8d0774f66ff9bc898fb64a24a7af28989f54f1
Kürşat Sarı <kursat.sari@metu.edu.tr>
2025-01-28 15:12:46 +0300

    fixed

--- clock.js
+++ clock.js
@@ -1,7 +1,24 @@
 let userName = document.querySelector("#myName")
 let clockInfo = document.querySelector("#myClock")
-var reqName = prompt("Lütfen İsminizi Giriniz")
+let reqName = prompt("Lütfen İsminizi Giriniz")
 
 function takeName() {
     userName.innerHTML = reqName
 }
+
+function showTime(){
+    const clock = new Date();
+    const hour = clock.getHours(); //saat
+    const time = clock.getMinutes(); //dakika
+    const day = clock.getDay();
+    const second = clock.getSeconds();
+    const daysOfWeek = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+
+    const formatDay=daysOfWeek[day];
+
+    myClock.innerHTML = `${hour}: ${time} : ${second} ${formatDay}`;
+    setInterval(showTime,1000);
+}
+
+takeName()
+showTime()
